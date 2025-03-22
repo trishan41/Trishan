701de7c668075dd6137dc13753020e90d1070b8f
trishan41 <trishanjitroy0@gmail.com>
2025-03-22 11:46:41 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-console.log("🔥 Website Loaded!");
-
-// Sample function to fetch visitor name
-function getVisitorName() {
-  const name = prompt("Enter your name:");
-  if (name) {
-    document.getElementById("visitorName").innerText = name;
-  }
-}
